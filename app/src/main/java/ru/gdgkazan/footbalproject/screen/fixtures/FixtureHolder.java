5733dece1d3df5d34b6054ec94ad495f44db2bc0
Алексей Antonchik <lexa_antonchik@mail.ru>
2016-09-20 02:53:17 +0300

    Task in progress, add layout. In Fixture model change String to Date. Add date format to Gson converter.

--- app/src/main/java/ru/gdgkazan/footbalproject/screen/fixtures/FixtureHolder.java
+++ app/src/main/java/ru/gdgkazan/footbalproject/screen/fixtures/FixtureHolder.java
@@ -3,8 +3,15 @@ package ru.gdgkazan.footbalproject.screen.fixtures;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import ru.gdgkazan.footbalproject.R;
 import ru.gdgkazan.footbalproject.model.content.Fixture;
 
 /**
@@ -12,6 +19,14 @@ import ru.gdgkazan.footbalproject.model.content.Fixture;
  */
 public class FixtureHolder extends RecyclerView.ViewHolder {
 
+    @BindView(R.id.awayTitle)
+    TextView mAwayText;
+    @BindView(R.id.homeTitle)
+    TextView mHomeText;
+    @BindView(R.id.result)
+    TextView mResultText;
+    @BindView(R.id.time)
+    TextView mTimeText;
 
 
     public FixtureHolder(View itemView) {
@@ -21,5 +36,34 @@ public class FixtureHolder extends RecyclerView.ViewHolder {
 
     public void bind(@NonNull Fixture fixture){
 
+        mAwayText.setText(fixture.getAwayTeamName());
+        mHomeText.setText(fixture.getHomeTeamName());
+
+        if(fixture.getResult() != null && fixture.getResult().getGoalsHomeTeam() != null && fixture.getResult().getGoalsAwayTeam() != null) {
+            mResultText.setText(fixture.getResult().toString());
+            mResultText.setAlpha(0.87f);
+        }else {
+            mResultText.setText("0 : 0");
+            mResultText.setAlpha(0.54f);
+        }
+
+        if(Calendar.getInstance().getTime().after(fixture.getDate())){
+            mTimeText.setText(mTimeText.getContext().getResources().getString(R.string.fixture_end));
+        }else {
+            mTimeText.setText(getStringDate(fixture.getDate()));
+        }
+    }
+
+    @NonNull
+    public String getStringDate(@NonNull Date date){
+        String result = "";
+        try {
+            String dateFormatString = "dd.MM.yyyy";
+            SimpleDateFormat df = new SimpleDateFormat(dateFormatString);
+            result = df.format(date);
+        }catch (Exception ex) {
+
+        }
+        return result;
     }
 }
