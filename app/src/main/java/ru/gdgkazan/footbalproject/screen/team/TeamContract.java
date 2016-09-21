9271207794c377157cba1c84860ad1d1427b8d52
stilroof <stilroof@gmail.com>
2016-09-21 17:12:49 +0300

    small fixes

--- app/src/main/java/ru/gdgkazan/footbalproject/screen/team/TeamContract.java
+++ app/src/main/java/ru/gdgkazan/footbalproject/screen/team/TeamContract.java
@@ -1,5 +1,7 @@
 package ru.gdgkazan.footbalproject.screen.team;
 
+import android.support.annotation.NonNull;
+
 import ru.gdgkazan.footbalproject.model.content.Team;
 import ru.gdgkazan.footbalproject.screen.loading.LoadingView;
 
@@ -7,13 +9,20 @@ import ru.gdgkazan.footbalproject.screen.loading.LoadingView;
  * Created by Sergei Riabov
  */
 public interface TeamContract {
+
     interface View extends LoadingView {
-        void showTeam(Team team);
+
+        void showTeam(@NonNull Team team);
+
         void showError();
+
     }
 
     interface UserActionListener {
-        void init(String teamName);
+
+        void init(@NonNull String teamName);
+
         void reload();
+
     }
 }
