f2d2b8ac5a8924dc70b8c21a0cd40aa1871770e4
Sergei Mikevich <sanfranciscocity@mail.ru>
2016-09-21 15:54:18 +0300

    created adapter & view holder for recycler view

--- app/src/main/java/ru/gdgkazan/footbalproject/screen/table/TableFragment.java
+++ app/src/main/java/ru/gdgkazan/footbalproject/screen/table/TableFragment.java
@@ -1,6 +1,7 @@
 package ru.gdgkazan.footbalproject.screen.table;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,34 +11,26 @@ import android.widget.Toast;
 
 import java.util.List;
 
-import ru.arturvasilov.rxloader.LifecycleHandler;
-import ru.arturvasilov.rxloader.LoaderLifecycleHandler;
 import ru.gdgkazan.footbalproject.R;
 import ru.gdgkazan.footbalproject.model.content.Standings;
+import ru.gdgkazan.footbalproject.screen.table.adapter.StandingsAdapter;
 import ru.gdgkazan.footbalproject.utils.Dialog;
 
 /**
  * Created by Alexey Antonchik on 18.09.16.
  */
-public class TableFragment extends Fragment implements TableContract.View {
-
-    private TablePresenter mPresenter;
+public class TableFragment extends Fragment implements TableContract.View, StandingsAdapter.OnItemClick {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_table, null);
-
-        LifecycleHandler lifecycleHandler = LoaderLifecycleHandler.create(getActivity(), getLoaderManager());
-        mPresenter = new TablePresenter(this, lifecycleHandler);
-        mPresenter.init();
-
         return view;
     }
 
     @Override
     public void showTable(List<Standings> standingsList) {
-        //TODO: show data in recycler view
+        //TODO: show table in recycler view
     }
 
     @Override
@@ -62,5 +55,10 @@ public class TableFragment extends Fragment implements TableContract.View {
     @Override
     public void showToastMessage(String message) {
         Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemClick(@NonNull Standings standing) {
+        Toast.makeText(getActivity(), "Pressed on standings!", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/ru/gdgkazan/footbalproject/screen/table/adapter/StandingsHolder.java
@@ -0,0 +1,100 @@
+package ru.gdgkazan.footbalproject.screen.table.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ru.gdgkazan.footbalproject.R;
+import ru.gdgkazan.footbalproject.model.content.Standings;
+
+/**
+ * Created by mikes on 21.09.16.
+ */
+
+public class StandingsHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.standingsParentRank)
+    TextView mStandingsParentRank;
+
+    @BindView(R.id.standingsParentTeam)
+    TextView mStandingsParentTeam;
+
+    @BindView(R.id.standingsParentWins)
+    TextView mStandingsParentWins;
+
+    @BindView(R.id.standingsParentDraws)
+    TextView mStandingsParentDraws;
+
+    @BindView(R.id.standingsParentLosses)
+    TextView mStandingsParentLosses;
+
+    @BindView(R.id.standingsParentPoints)
+    TextView mStandingsParentPoints;
+
+    @BindView(R.id.standingsChildHomeWins)
+    TextView mStandingsChildHomeWins;
+
+    @BindView(R.id.standingsChildHomeDraws)
+    TextView mStandingsChildHomeDraws;
+
+    @BindView(R.id.standingsChildHomeLosses)
+    TextView mStandingsChildHomeLosses;
+
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
+
+    @BindView(R.id.standingsChildGoalsDifference)
+    TextView mStandingsChildGoalsDifference;
+
+    @BindView(R.id.standingsChildHomeGoalsScored)
+    TextView mStandingsChildHomeGoalsScored;
+
+    @BindView(R.id.standingsChildAwayGoalsScored)
+    TextView mStandingsChildAwayGoalsScored;
+
+    @BindView(R.id.standingsChildHomeGoalsAgainst)
+    TextView mStandingsChildHomeGoalsAgainst;
+
+    @BindView(R.id.standingsChildAwayGoalsAgainst)
+    TextView mStandingsChildAwayGoalsAgainst;
+
+    @BindView(R.id.standingsChildAwayWins)
+    TextView mStandingsChildAwayWins;
+
+    @BindView(R.id.standingsChildAwayDraws)
+    TextView mStandingsChildAwayDraws;
+
+    @BindView(R.id.standingsChildAwayLosses)
+    TextView mStandingsChildAwayLosses;
+
+    public StandingsHolder(@NonNull  View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(@NonNull Standings standings){
+        mStandingsParentRank.setText(String.valueOf(standings.getPosition()));
+        mStandingsParentTeam.setText(standings.getTeamname());
+        mStandingsParentWins.setText(String.valueOf(standings.getWins()));
+        mStandingsParentDraws.setText(String.valueOf(standings.getDraws()));
+        mStandingsParentLosses.setText(String.valueOf(standings.getLosses()));
+        mStandingsParentPoints.setText(String.valueOf(standings.getPoints()));
+        mStandingsChildHomeWins.setText(String.valueOf(standings.getStandingsDetailsHome().getWins()));
+        mStandingsChildHomeDraws.setText(String.valueOf(standings.getStandingsDetailsHome().getDraws()));
+        mStandingsChildHomeLosses.setText(String.valueOf(standings.getStandingsDetailsHome().getLosses()));
+        mProgressBar.setProgress((100 * standings.getGoals() / (standings.getGoals() + standings.getGoalsAgainst())));
+        mStandingsChildGoalsDifference.setText(String.valueOf(standings.getGoalDifference()));
+        mStandingsChildHomeGoalsScored.setText(String.valueOf(standings.getStandingsDetailsHome().getGoals()));
+        mStandingsChildAwayGoalsScored.setText(String.valueOf(standings.getStandingsDetailsAway().getGoals()));
+        mStandingsChildHomeGoalsAgainst.setText(String.valueOf(standings.getStandingsDetailsHome().getGoalsAgainst()));
+        mStandingsChildAwayGoalsAgainst.setText(String.valueOf(standings.getStandingsDetailsAway().getGoalsAgainst()));
+        mStandingsChildAwayWins.setText(String.valueOf(standings.getStandingsDetailsAway().getWins()));
+        mStandingsChildAwayDraws.setText(String.valueOf(standings.getStandingsDetailsAway().getDraws()));
+        mStandingsChildAwayLosses.setText(String.valueOf(standings.getStandingsDetailsAway().getLosses()));
+    }
+
+}
