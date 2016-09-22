0b2f184f1d7d93ab59624bb788683fc4071fa11e
stilroof <stilroof@gmail.com>
2016-09-22 14:29:58 +0300

    Replaced player birth date type from String to Date

--- app/src/main/java/ru/gdgkazan/footbalproject/screen/team/PlayersHolder.java
+++ app/src/main/java/ru/gdgkazan/footbalproject/screen/team/PlayersHolder.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import ru.gdgkazan.footbalproject.R;
@@ -45,11 +49,21 @@ class PlayersHolder extends RecyclerView.ViewHolder {
 
         mNameText.setText(player.getName());
         mPositionText.setText(player.getPosition());
-        mBirthText.setText(player.getDateOfBirth());
+        mBirthText.setText(mBirthText.getResources().getString(R.string.player_age, getPlayerAge(player.getDateOfBirth())));
         if(player.getJerseyNumber() != null) {
             mNumberText.setText(String.valueOf(player.getJerseyNumber()));
         } else {
             mNumberText.setText(R.string.not_available);
         }
+    }
+
+    private int getPlayerAge(Date birthDate) {
+        Calendar todayCal = GregorianCalendar.getInstance();
+        Calendar bitrhCal = GregorianCalendar.getInstance();
+        bitrhCal.setTime(birthDate);
+
+        todayCal.add(Calendar.DAY_OF_YEAR, -bitrhCal.get(Calendar.DAY_OF_YEAR));
+
+        return todayCal.get(Calendar.YEAR) - bitrhCal.get(Calendar.YEAR);
     }
 }
